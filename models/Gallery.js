4121a9e44ce51a41fcc1a5007631ed8f5783430c
samdone763 <samdone763@gmail.com>
2026-02-24 01:07:35 +0300

    Update Gallery.js

--- models/Gallery.js
+++ models/Gallery.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const GallerySchema = new mongoose.Schema({
   caption: { type: String, required: true },
   imageData: { type: String, required: true },
+  mimeType: { type: String, default: 'image/jpeg' },
   createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Gallery', GallerySchema);
